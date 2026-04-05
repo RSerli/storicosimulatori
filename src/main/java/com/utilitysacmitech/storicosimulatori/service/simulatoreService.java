2f8ecf6980a1ae394fd2e5d98a5475b46c5a2fe6
Rserli <riccardoserli.95@gmail.com>
2026-04-05 22:05:21 +0200

    Removed Async on checkConnection

--- src/main/java/com/utilitysacmitech/storicosimulatori/service/simulatoreService.java
+++ src/main/java/com/utilitysacmitech/storicosimulatori/service/simulatoreService.java
@@ -3,7 +3,6 @@ package com.utilitysacmitech.storicosimulatori.service;
 import java.net.InetAddress;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.utilitysacmitech.storicosimulatori.model.simulatore;
